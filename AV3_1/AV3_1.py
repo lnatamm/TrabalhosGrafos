60ee8dc020924d561588cc6c0436b93eecb9c157
Levi <lnatamm25@gmail.com>
2024-11-30 17:46:04 -0300

    Implementando leitura de arquivo

--- AV3_1/AV3_1.py
+++ AV3_1/AV3_1.py
@@ -1,1 +1,24 @@
-print("Hello World")
+import os
+
+#Remove o caractere de quebra de linha
+def removeLineBreak(variable):
+    variable = variable.split()
+    variable = int(variable[0])
+    return variable
+
+#Lê a matriz de adjacência do arquivo "adjMatrix.txt"
+def readFile():
+    filePath = os.getcwd() + "\\AV3_1\\adjMatrix.txt"
+    file = open(filePath)
+    n = file.readline()
+    n = removeLineBreak(n)
+    line = file.readline().split()
+    i = line[0]
+    j = line[0]
+    G = []
+    for i in range(n):
+        G.append([])
+        line = file.readline().split()
+        for j in range(n):
+            G[i].append(float(line[j]))
+    return G
